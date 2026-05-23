0315530d0e358767c670609d1a61632b5479e8f4
HOME-PC\Olga <tumanina@yandex.ru>
2026-05-24 00:10:49 +0300

    Added csv files parser

---
+++ MemoryPlayground.Parser/Program.cs
@@ -0,0 +1,41 @@
+﻿var result = new List<Data>();
+
+foreach (var line in File.ReadAllLines("test.csv"))
+{
+    ReadOnlySpan<char> span = line;
+    
+    TryReadColumn(ref span, out var idSpan);
+    TryReadColumn(ref span, out var nameSpan);
+    TryReadColumn(ref span, out var countrySpan);
+    TryReadColumn(ref span, out var amountSpan);
+
+    result.Add(new Data(int.Parse(idSpan), nameSpan.ToString(), countrySpan.ToString(), decimal.Parse(amountSpan)));
+}
+
+static bool TryReadColumn(ref ReadOnlySpan<char> span, out ReadOnlySpan<char> column)
+{
+    if (span.IsEmpty)
+    {
+        column = default;
+        return false;
+    }
+
+    int commaIndex = span.IndexOf(',');
+
+    if (commaIndex == -1)
+    {
+        column = span;
+        span = ReadOnlySpan<char>.Empty;
+        return true;
+    }
+
+    column = span[..commaIndex];
+    span = span[(commaIndex + 1)..];
+
+    return true;
+}
+
+record Data(long Id, string Name, string Country, decimal Amount);
+
+
+
